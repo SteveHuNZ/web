21b2f2a1114269097641ca00db578fd9e8c31a03
william <william@laptop>
2024-01-25 14:56:51 +1300

    few handlebars

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Load a .env file if one exists
-// require('dotenv').config()
+require('dotenv').config()
 
 const express = require("express");
 const handlebars = require("express-handlebars");
@@ -10,21 +10,28 @@ const port = process.env.EXPRESS_PORT || 3000;
 
 // Setup Handlebars
 app.engine("handlebars", handlebars.create({
-    defaultLayout:"home"
+    defaultLayout:"main_layout"
 }).engine);
 app.set("view engine", "handlebars");
 
 // Set up to read POSTed form data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({}));
+//set public folder
+const path = require("path");
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // TODO: Your app here
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', { home: true });
+});
+
+app.get('/login', (req, res) => {
+    res.render('login', { login: true });
 });
 app.get('/register', (req, res) => {
-    res.render('register');
+    res.render('register', { register: true });
 });
 
 
