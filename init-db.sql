a016d7be37c3876f1179c0aecc175925efabef6a
william <william@laptop>
2024-02-05 10:05:38 +1300

    changed the structure of the database

--- init-db.sql
+++ init-db.sql
@@ -1,18 +1,21 @@
-
 drop table if exists web_users;
+select * from web_users;
 
 create table if not exists web_users (
-                                            id int not null auto_increment,
-                                            username varchar(64) not null,
-    password varchar(64) not null,
-    name varchar(64),
-    primary key (id),
-    constraint unique_username unique (username)
-    );
+                                           id int not null auto_increment,
+                                           username varchar(64) not null,
+                                           password varchar(64) not null,
 
-insert into web_users (id, username, password, name) values
-                                                            (1, 'user1', '123456', 'Steve'),
-                                                            (2, 'user2', '123456', 'William'),
-(3, 'user3', '123456', 'Katelyn');
+                                           name varchar(64),
+                                           description TEXT,
+                                           birthday DATE,
 
-select * from web_users;
+                                           primary key (id)
+
+);
+
+INSERT INTO web_users (id, username, password, name, description, birthday)
+VALUES
+    (1, 'user11', 'password123', 'John Doe', 'A web developer with a passion for tech.', '1985-04-12'),
+    (2,'user12', 'password456', 'Jane Doe', 'Enthusiastic software engineer and tech lover.', '1990-07-08'),
+    (3,'user13', 'password789', 'Mike Smith', 'Experienced database administrator.', '1978-11-22');
