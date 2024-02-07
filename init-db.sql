bea75e6dc9275eea8a3f0997a1280d6b97ddc9e1
william <william@laptop>
2024-02-08 10:16:23 +1300

    refined the home.handlebars and amin.handlebars

--- init-db.sql
+++ init-db.sql
@@ -1,21 +1,26 @@
-drop table if exists web_users;
-select * from web_users;
 
-create table if not exists web_users (
-                                           id int not null auto_increment,
-                                           username varchar(64) not null,
-                                           password varchar(64) not null,
+drop table if exists lab_15_users;
+select * from lab_15_users;
 
-                                           name varchar(64),
-                                           description TEXT,
-                                           birthday DATE,
+create table if not exists lab_15_users (
+                                            id int not null auto_increment,
+                                            username varchar(64) not null,
+                                            password varchar(64) not null,
 
-                                           primary key (id)
+                                            name varchar(64),
+                                            primary key (id)
 
 );
 
-INSERT INTO web_users (id, username, password, name, description, birthday)
+
+ALTER TABLE lab_15_users
+    ADD COLUMN description TEXT,
+    ADD COLUMN birthday DATE;
+
+
+
+INSERT INTO lab_15_users (id, username, password, name, description, birthday)
 VALUES
-    (1, 'user11', 'password123', 'John Doe', 'A web developer with a passion for tech.', '1985-04-12'),
-    (2,'user12', 'password456', 'Jane Doe', 'Enthusiastic software engineer and tech lover.', '1990-07-08'),
-    (3,'user13', 'password789', 'Mike Smith', 'Experienced database administrator.', '1978-11-22');
+    (11, 'user11', 'password123', 'John Doe', 'A web developer with a passion for tech.', '1985-04-12'),
+    (12,'user12', 'password456', 'Jane Doe', 'Enthusiastic software engineer and tech lover.', '1990-07-08'),
+    (13,'user13', 'password789', 'Mike Smith', 'Experienced database administrator.', '1978-11-22');
